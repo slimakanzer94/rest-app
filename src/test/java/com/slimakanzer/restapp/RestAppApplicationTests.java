af1e4580355cf6021f0eb57592c419f2009a0a54
slimakanzer94 <ego313666@mail.ru>
2020-01-21 23:34:18 +0500

    Add pom.xml

---
+++ src/test/java/com/slimakanzer/restapp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slimakanzer.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
